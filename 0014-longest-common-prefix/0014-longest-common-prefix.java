d1f182834ef90c9b51db89cbcbc60584e8f2aa19
KothaiSundar <131440849+KothaiSundar@users.noreply.github.com>
2024-02-06 16:55:25 +0100

    Time: 1 ms (80.62%), Space: 41.5 MB (49.00%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,20 +4,19 @@ class Solution {
             return "";
         }
         
-        // Iterate through characters of the first string
         for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
             
-            // Check if the character is present in the same position for all other strings
+          
             for (int j = 1; j < strs.length; j++) {
                 if (i >= strs[j].length() || strs[j].charAt(i) != c) {
-                    // If not present or not matching, return the prefix up to this point
+                   
                     return strs[0].substring(0, i);
                 }
             }
         }
         
-        // If all strings have been iterated through and no mismatch found, return the first string
+
         return strs[0];
         
     }
